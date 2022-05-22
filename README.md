2877427e3cf2b86bfb86e935eadd91807d33d2f2
Kübra Selçuk <70449658+KubraSelcuk@users.noreply.github.com>
2022-05-22 14:12:41 +0300

    ilk commit

---
+++ README.md
@@ -0,0 +1,15 @@
+Projeyi hazırlayanlar:
+-Kübra Selçuk
+-Zehra Abuşak
+-Batuhan Berk Karadere
+-Serhat Yıldız
+
+Flutter ile Sqlit'a bağlanarak veri ekleme. Bu projede Todo list(yapılacaklar listesi) kodlanmıştır.
+
+İlk arayüzde Title, Description, ve Date verileri girilip Dropdown listesinden kategori seçilmektedir.
+
+Ardından Save butonuna basınca veri Drawer menüdeki seçilen kategoriye göre listeye eklenmiş oluyor
+ve girilen veri aynı zamanda Anasayfaya eklenmiş oluyor.
+
+Drawer menüde Avatar fotoğrafı ve Kullanıcı Adı vardır.
+Home ve Kategoriler seçilebilmektedir.
---
+++ lib/helpers/drawer_navigation.dart
@@ -0,0 +1,83 @@
+import 'package:flutter/material.dart';
+import 'package:flutterapp/screen/categories_screen.dart';
+import 'package:flutterapp/screen/home_screen.dart';
+
+import '../screen/todos_category.dart';
+import '../services/category_service.dart';
+
+class DrawerNavigation extends StatefulWidget {
+  const DrawerNavigation({Key? key}) : super(key: key);
+
+  @override
+  State<DrawerNavigation> createState() => _DrawerNavigationState();
+}
+
+class _DrawerNavigationState extends State<DrawerNavigation> {
+  //
+  List<Widget> _categoryList = List<Widget>.empty(growable: true);
+
+  CategoryService _categoryService = CategoryService();
+
+  @override
+  initState() {
+    super.initState();
+    getAllCategories();
+  }
+
+  getAllCategories() async {
+    var categories = await _categoryService.readCategories();
+
+    categories.forEach((category) {
+      setState(() {
+        _categoryList.add(InkWell(
+          onTap: () => Navigator.push(
+              context,
+              new MaterialPageRoute(
+                  builder: (context) => new TodosByCategory(
+                        category: category['name'],
+                      ))),
+          child: ListTile(
+            title: Text(category['name']),
+          ),
+        ));
+      });
+    });
+  }
+
+  //
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Drawer(
+        child: ListView(
+          children: <Widget>[
+            UserAccountsDrawerHeader(
+              currentAccountPicture: CircleAvatar(
+                backgroundImage: NetworkImage(
+                    'https://cdn.pixabay.com/photo/2021/08/20/15/22/honda-civic-si-6560708__480.jpg'),
+              ),
+              accountName: Text('test'),
+              accountEmail: Text('admin.admin'),
+              decoration: BoxDecoration(color: Colors.blue),
+            ),
+            ListTile(
+              leading: Icon(Icons.home),
+              title: Text('Home'),
+              onTap: () => Navigator.of(context)
+                  .push(MaterialPageRoute(builder: (context) => HomeScreen())),
+            ),
+            ListTile(
+              leading: Icon(Icons.view_list),
+              title: Text('Categories'),
+              onTap: () => Navigator.of(context).push(
+                  MaterialPageRoute(builder: (context) => CategoriesScreen())),
+            ),
+            Column(
+              children: _categoryList,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
